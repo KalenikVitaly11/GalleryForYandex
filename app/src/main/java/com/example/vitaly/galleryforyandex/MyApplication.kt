fb02d5f748204b009d819c0ffd77198076764146
Vitaly <kvitaly21@yandex.ru>
2018-05-06 13:55:07 +0300

    Убрал LeakCanary

--- app/src/main/java/com/example/vitaly/galleryforyandex/MyApplication.kt
+++ app/src/main/java/com/example/vitaly/galleryforyandex/MyApplication.kt
@@ -1,7 +1,6 @@
 package com.example.vitaly.galleryforyandex
 
 import android.app.Application
-import com.squareup.leakcanary.LeakCanary
 import io.realm.Realm
 
 
@@ -10,9 +9,5 @@ class MyApplication : Application(){
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        if(LeakCanary.isInAnalyzerProcess(this)){
-            return
-        }
-        LeakCanary.install(this)
     }
 }
